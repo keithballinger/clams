2db6d377e663ebaf22eed7bb8d7e47e18959727a
Jim Brusstar <jim.brusstar@gmail.com>
2014-11-12 13:55:24 -0800

    Modify project file for internal deployment

--- project.clj
+++ project.clj
@@ -1,16 +1,21 @@
-(defproject clams "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clams
+  "0.1.0-SNAPSHOT"
+  :description "Clams"
+  :url "http://github.com/standardtreasury-internal/clams"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.macro "0.1.5"]
-                 ;;[camel-snake-kebab "0.2.5"]
-                 [clout "2.0.0"]
+  :dependencies [[clout "2.0.0"]
                  [compojure "1.2.0"]
                  [http-kit "2.1.17"]
                  [metosin/ring-http-response "0.5.1"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.macro "0.1.5"]
                  [prismatic/schema "0.3.2"]
                  [ring "1.3.1"]
-                 [ring/ring-json "0.3.1"]
-                 [ring-mock "0.1.5"]])
+                 [ring-mock "0.1.5"]
+                 [ring/ring-json "0.3.1"]]
+  :repositories {"internal" {:url "s3p://standard-releases/releases/"
+                             :username :env
+                             :passphrase :env
+                             :sign-releases false}}
+  :plugins [[s3-wagon-private "1.1.2"]])

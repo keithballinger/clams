90d0e7b8e68bb6d964a9227d9a5e9c3cf5743073
Jim Brusstar <jim.brusstar@gmail.com>
2014-11-13 13:13:08 -0800

    Add lein eastwood and test-out plugins
    Supports ST internal arc lint and unit workflows.

--- project.clj
+++ project.clj
@@ -4,18 +4,26 @@
   :url "http://github.com/standardtreasury-internal/clams"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clout "2.0.0"]
-                 [compojure "1.2.0"]
-                 [http-kit "2.1.17"]
-                 [metosin/ring-http-response "0.5.1"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.macro "0.1.5"]
-                 [prismatic/schema "0.3.2"]
-                 [ring "1.3.1"]
-                 [ring-mock "0.1.5"]
-                 [ring/ring-json "0.3.1"]]
+  :dependencies
+    [
+     [clout "2.0.0"]
+     [compojure "1.2.0"]
+     [http-kit "2.1.17"]
+     [metosin/ring-http-response "0.5.1"]
+     [org.clojure/clojure "1.6.0"]
+     [org.clojure/tools.macro "0.1.5"]
+     [prismatic/schema "0.3.2"]
+     [ring "1.3.1"]
+     [ring-mock "0.1.5"]
+     [ring/ring-json "0.3.1"]
+    ]
   :repositories {"internal" {:url "s3p://standard-releases/releases/"
                              :username :env
                              :passphrase :env
                              :sign-releases false}}
-  :plugins [[s3-wagon-private "1.1.2"]])
+  :plugins
+    [
+     [jonase/eastwood "0.1.5"]
+     [lein-test-out "0.3.1"]
+     [s3-wagon-private "1.1.2"]
+    ])

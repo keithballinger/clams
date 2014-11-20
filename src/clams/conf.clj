367d371e9da7d621054eda3bff635e7b3cab93b8
Jim Brusstar <jim.brusstar@gmail.com>
2014-11-20 14:39:48 -0800

    Allow CLAMS_ENV to be set using a Java system property
    Summary:
    In general I dislike Java system properties. They seem to be a gratuitous take
    on environment variables. And they're mutable--so what's the point?
    Unfortunately Leiningen allows them to be set per-profile, while the real env
    vars get no love.
    
    In order for apps to support a Clams "testing" config, without having to resort
    to other libs or hacks, we need a way to set `CLAMS_ENV=test` from the
    Leiningen test runner. This diff extends conf to look for a property named
    `clams.env` that can be used in lieu of the equivalent env var.
    
    Test Plan:
      lein test
    
    Reviewers: #standard_treasury, ctdean
    
    Reviewed By: #standard_treasury, ctdean
    
    Subscribers: ctdean
    
    Differential Revision: http://phabricator.corp.standardtreasury.com/D3841

--- src/clams/conf.clj
+++ src/clams/conf.clj
@@ -1,6 +1,6 @@
 (ns clams.conf
-  "Provides app configuration. Clams app configuration is read both from
-  EDN-formatted files compiled into resources and environment variables.
+  "Provides app configuration. Clams app configuration is read from EDN files
+  compiled into resources, environment variables, and Java system properties.
   This namespace contains both the infrastructure for loading these values
   and accessing them at run-time.
 
@@ -10,16 +10,22 @@
   `/resources/conf/default.edn`.
 
   Apps may also add environment-specific config files. These are selectable at
-  run-time by setting the special environment variable `CLAMS_ENV`. Their values
-  are merged on top of, and take precedence over, the default and base configs.
-  For example, to activate a production-specific configuration, one might run
-  the command: `CLAMS_ENV=prod lein run`. The value of `CLAMS_ENV` can be an
-  arbitrary string; the value of that string determines which config is loaded.
+  run-time by setting the special environment variable `CLAMS_ENV` (or the Java
+  property `clams.env`). These config values are merged on top of, and take
+  precedence over, the default and base configs. For example, to activate a
+  production-specific configuration, one might run the command:
+  `CLAMS_ENV=prod lein run`. The value of `CLAMS_ENV` can be an arbitrary
+  string; the value of that string determines which config is loaded.
 
-  Environment variables are also merged into the config. These take the highest
+  Environment variables are also merged into the config. These take a higher
   precedence and can override any file's value. Names of environment variables
   are normalized from UPPER_UNDERSCORE_CASE strings to more Clojure-esque
   lower-dash-case keywords.
+
+  Finally, Java system properties are also merged into the config. These take
+  the highest precedence of all, and can override any other config value.
+  Property names are normalized from lower.dot.case to the typical
+  lower-dash-case, just like environment variables.
   "
   (:refer-clojure :rename {get core-get})
   (:require [clojure.edn :as edn]
@@ -28,10 +34,11 @@
 
 (def ^:private conf (atom nil))
 
-(defn- normalize-env-var-name
-  "Environment variables are often written in UPPER_UNDERSCORE_CASE.
-  Since Clojure keywords are ordinarily written in lower-dash-case,
-  this function normalizes them to match."
+(defn- normalize-var-name
+  "Environment variables are often written in UPPER_UNDERSCORE_CASE. Likewise,
+  Java system properties are usually written in lower.dot.case. Since Clojure
+  keywords are ordinarily written in lower-dash-case, this function normalizes
+  them all to match."
   [name]
   (if-not (nil? name)
     (-> name
@@ -40,23 +47,31 @@
         (string/replace "." "-")
         keyword)))
 
-(defn- getenv
+(defn- normalize-var-map
+  "Normalizes a config var map."
+  [vmap]
+  (into {} (for [[k v] vmap]
+    [(normalize-var-name k) v])))
+
+(defn- sys-getenv
   "Wrapper around System/getenv. Makes it easier to mock in tests."
   []
   (System/getenv))
 
-(defn- read-env
-  "Reads the environment and processes it into a normalized map."
+(defn- sys-getproperties
+  "Wrapper around System/getProperties. Makes it easier to mock in tests."
   []
-  (into {} (for [[k v] (getenv)]
-    [(normalize-env-var-name k) v])))
+  (System/getProperties))
 
-(defn- get-clams-env
-  "Parses from a normalized env map the special CLAMS_ENV var. The value
-  of this var informs which additional config files should be loaded."
-  [env]
-  (when-let [cenv (:clams-env env)]
-    (string/lower-case cenv)))
+(defn- read-env
+  "Reads environment variable configs."
+  []
+  (normalize-var-map (sys-getenv)))
+
+(defn- read-props
+  "Reads Java system properties configs."
+  []
+  (normalize-var-map (sys-getproperties)))
 
 (defn- read-config-file
   "Reads from resources the config file of the given name and processes it
@@ -68,16 +83,25 @@
         {}  ;; Config file not found.
         (edn/read-string (slurp resource))))))
 
+(defn- get-clams-env
+  "Parses the special CLAMS_ENV var from config maps. The value of
+  this var informs which additional config files should be loaded."
+  [& cfgs]
+  (when-let [clams-env (some identity (map :clams-env cfgs))]
+    (string/lower-case clams-env)))
+
 (defn load!
   "Loads the app config. Usually you won't need to call this directly as
   the config will be automatically loaded when you attempt to access it."
   []
-  (let [env  (read-env)
-        cenv (get-clams-env env)]
+  (let [props     (read-props)
+        env       (read-env)
+        clams-env (get-clams-env props env)]
     (reset! conf (merge (read-config-file "base")
                         (read-config-file "default")
-                        (read-config-file cenv)
-                        env))))
+                        (read-config-file clams-env)
+                        env
+                        props))))
 
 (defn unload!
   "Unloads the app config. This exists mainly to ease testing
--- test/clams/test/conf_test.clj
+++ test/clams/test/conf_test.clj
@@ -33,10 +33,11 @@
     (throw (Exception. "Unknown fixture."))))
 
 (defn wrap-fixtures
-  [env f]
-  (with-redefs [clojure.java.io/resource identity
-                clojure.core/slurp       stub-slurp
-                clams.conf/getenv        (fn [] env)]
+  [props env f]
+  (with-redefs [clojure.java.io/resource     identity
+                clojure.core/slurp           stub-slurp
+                clams.conf/sys-getenv        (fn [] env)
+                clams.conf/sys-getproperties (fn [] props)]
     (f)))
 
 (use-fixtures :each (fn [f]
@@ -53,35 +54,47 @@
   (is (= (conf/get :port) 5000)))
 
 (deftest get-from-default-test
-  (wrap-fixtures {} conf/load!)
+  (wrap-fixtures {} {} conf/load!)
   (is (= (conf/get :database-url) "sql://fake:1234/foobar"))
   (is (= (conf/get :log-level) :debug)))
 
 (deftest get-from-clams-env-test
   (testing "dev"
-    (wrap-fixtures {"CLAMS_ENV" "dev"} conf/load!)
+    (wrap-fixtures {} {"CLAMS_ENV" "dev"} conf/load!)
     (is (= (conf/get :database-url) "sql://fake:1234/devdb"))
     (conf/unload!))
   (testing "prod"
-    (wrap-fixtures {"clams.env" "PrOd"} conf/load!)
+    (wrap-fixtures {} {"clams.env" "PrOd"} conf/load!)
     (is (= (conf/get :database-url) "sql://fake:1234/proddb"))
+    (conf/unload!))
+  (testing "using system property"
+    (wrap-fixtures {"clams.env" "dev"} {} conf/load!)
+    (is (= (conf/get :database-url) "sql://fake:1234/devdb"))
     (conf/unload!)))
 
 (deftest get-from-env-test
-  (wrap-fixtures {"DATABASE_URL" "sql://dev.fake:1234/foobar"} conf/load!)
+  (wrap-fixtures {} {"DATABASE_URL" "sql://dev.fake:1234/foobar"} conf/load!)
   (is (= (conf/get :database-url) "sql://dev.fake:1234/foobar"))
   (is (= (conf/get-all) {:database-url "sql://dev.fake:1234/foobar"
                          :log-level    :debug
                          :port         5000})))
 
+(deftest get-from-props-test
+  (wrap-fixtures {"database.url" "sql://props:1234"}
+                 {"DATABASE_URL" "sql://dev.fake:1234/foobar"}
+                 conf/load!)
+  (is (= (conf/get :database-url) "sql://props:1234")))
+
 (deftest get-not-found-arg-test
-  (wrap-fixtures {} conf/load!)
+  (wrap-fixtures {} {} conf/load!)
   (is (= (conf/get :xxx) nil))
   (is (= (conf/get :xxx :foobar) :foobar)))
 
 (deftest get-all-test
-  (wrap-fixtures {} conf/load!)
+  (wrap-fixtures {"foo" "bar"} {"baz" "quux"} conf/load!)
   (is (= (conf/get-all)
          {:database-url "sql://fake:1234/foobar"
           :log-level    :debug
-          :port         5000})))
+          :port         5000
+          :foo          "bar"
+          :baz          "quux"})))
